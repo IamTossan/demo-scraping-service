0fcfaf4db5160199944f8c1f7efd8d5b5dbe32fc
Albert Oudompheng <tossanmusic@gmail.com>
2025-04-22 00:56:44 +0200

    refactor: refactor login form

--- invoice-front/app/routes/login.tsx
+++ invoice-front/app/routes/login.tsx
@@ -1,61 +1,101 @@
-import { Form, useNavigate } from "@remix-run/react";
+import { useNavigate } from "@remix-run/react";
 import { toast } from "sonner";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import { useSupabase } from "~/supabase.context";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+
+export const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(100),
+});
 
 export default function Login() {
   const supabase = useSupabase();
   const navigate = useNavigate();
 
-  const login = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: e.target.elements.email.value,
-      password: e.target.elements.password.value,
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof schema>) => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email: values.email,
+      password: values.password,
     });
-    console.log("data", data);
     if (error) {
       console.error("login error", error);
       toast.error(error.message);
     } else {
+      toast.info("Connection réussie");
       return navigate("/");
     }
   };
 
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-4">
-      <Form
-        onSubmit={login}
-        className="flex flex-col justify-between items-center h-80 p-8 border rounded-md"
-      >
-        <div className="flex flex-col items-center gap-2">
-          <h1 className="text-2xl font-bold mb-4">Connection</h1>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              className="text-right"
-              type="text"
-              id="email"
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col justify-between items-center h-96 w-xl p-8 border rounded-md"
+        >
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="text-2xl font-bold mb-4">Connection</h1>
+            <FormField
+              control={form.control}
               name="email"
-              placeholder="user@example.com"
+              render={({ field }) => (
+                <FormItem className="w-sm h-20">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="text-right"
+                      type="text"
+                      placeholder="user@example.com"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              className="text-right"
-              type="password"
-              id="password"
+            <FormField
+              control={form.control}
               name="password"
-              placeholder="password"
+              render={({ field }) => (
+                <FormItem className="w-sm h-20">
+                  <FormLabel>Mot de passe</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="text-right"
+                      type="password"
+                      placeholder="password"
+                    />
+                  </FormControl>
+                  <FormMessage className="relative" />
+                </FormItem>
+              )}
             />
           </div>
-        </div>
-        <Button type="submit" className="self-end">
-          Login
-        </Button>
+          <Button type="submit" className="self-end">
+            Login
+          </Button>
+        </form>
       </Form>
     </div>
   );
